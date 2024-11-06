1ce6ccf92ab2faaa7af84c45374207def15df7eb
Hongbo <ketchup@sjtu.edu.cn>
2024-11-06 22:33:36 +0800

    checking velocity

--- code/config.py
+++ code/config.py
@@ -3,10 +3,13 @@ import numpy as np
 
 ################ red shift binning
 
-z_max = 5.0 #highest redshift.
-z_min = 0.2 #lowest redshift.
+z_max = 0.7 #highest redshift.
+z_min = 0.4 #lowest redshift.
 
-N_bins = 10 #number of red shift bins, uniform in conformal distance.
+N_bins = 4 #number of red shift bins, uniform in conformal distance.
+
+# using the z_boundaries for F. McCarthy et al. 2024
+# z_boundaries = [0.4, 0.49, 0.57, 0.64, 0.7]
 
 ################ halomodel
 use_halomodel = False
--- code/get_spectra.sh
+++ code/get_spectra.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-python spectra.py -t1 vr -t2 vr -lmax 6144 
-python spectra.py -t1 vr -t2 taud -lmax 6144 
-python spectra.py -t1 vr -t2 g -lmax 6144 
-python spectra.py -t1 taud -t2 taud -lmax 6144 
-python spectra.py -t1 taud -t2 g -lmax 6144 
-python spectra.py -t1 g -t2 g -lmax 6144 
-python spectra.py -t1 isw_lin -t2 g -lmax 6144 
-python spectra.py -t1 isw_lin -t2 isw_lin -lmax 6144 
-python spectra.py -t1 tSZ -t2 g -lmax 6144 
-python spectra.py -t1 CIB -t2 g -lmax 6144 
-python spectra.py -t1 tSZ -t2 tSZ -lmax 6144 
-python spectra.py -t1 tSZ -t2 CIB -lmax 6144 
-python spectra.py -t1 CIB -t2 CIB -lmax 6144 
-python spectra.py -t1 vt -t2 vt -lmax 6144 
-python spectra.py -t1 vt -t2 ml -lmax 6144 
-python spectra.py -t1 vt -t2 g -lmax 6144 
-python spectra.py -t1 ml -t2 ml -lmax 6144 
-python spectra.py -t1 ml -t2 g -lmax 6144 
-python spectra.py -t1 pCMB -t2 pCMB -lmax 6144
-python spectra.py -t1 lensing -t2 g -lmax 6144  
-python spectra.py -t1 lensing -t2 lensing -lmax 6144  
+python spectra.py -t1 vr -t2 vr -lmax 4000
+python spectra.py -t1 vr -t2 taud -lmax 4000
+python spectra.py -t1 vr -t2 g -lmax 4000
+python spectra.py -t1 taud -t2 taud -lmax 4000
+python spectra.py -t1 taud -t2 g -lmax 4000
+python spectra.py -t1 g -t2 g -lmax 4000
+
+
+# python spectra.py -t1 isw_lin -t2 g -lmax 4000
+# python spectra.py -t1 isw_lin -t2 isw_lin -lmax 4000
+# python spectra.py -t1 tSZ -t2 g -lmax 4000
+# python spectra.py -t1 CIB -t2 g -lmax 4000
+# python spectra.py -t1 tSZ -t2 tSZ -lmax 4000
+# python spectra.py -t1 tSZ -t2 CIB -lmax 4000
+# python spectra.py -t1 CIB -t2 CIB -lmax 4000
+# python spectra.py -t1 vt -t2 vt -lmax 4000
+# python spectra.py -t1 vt -t2 ml -lmax 4000
+# python spectra.py -t1 vt -t2 g -lmax 4000
+# python spectra.py -t1 ml -t2 ml -lmax 4000
+# python spectra.py -t1 ml -t2 g -lmax 4000
+# python spectra.py -t1 pCMB -t2 pCMB -lmax 4000
+# python spectra.py -t1 lensing -t2 g -lmax 4000
+# python spectra.py -t1 lensing -t2 lensing -lmax 4000
