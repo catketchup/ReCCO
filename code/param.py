2462e25c54fbaee841b609cbc63ac6fbecf13647
Hongbo <ketchup@sjtu.edu.cn>
2024-11-09 22:45:26 +0800

    symmetry problem

--- code/param.py
+++ code/param.py
@@ -11,11 +11,11 @@ r_o = 15000
 x_c = 0
 r_H = c/H0
 # initial potential from bubble collision psi_i parametried by A and B, Eq.2.2
-# A = 1.e-4 # 1-sigma limit
-# B = 0
+A = 1.e-4 # 1-sigma limit
+B = 0
 
-A =  0# 1-sigma limit
-B = 1.e-4
+# A =  0# 1-sigma limit
+# B = 1.e-4
 
 # A = 0
 # B = 1.e-4 # 1-sigma limit
@@ -23,8 +23,8 @@ B = 1.e-4
 # A = 1
 # B = 1
 # need a resolution of the 3-d grid?
-d = 1000
-N = int(2*r_o/d) +1
+# d = 1000
+# N = int(2*r_o/d) +1
 
 curvature_ini_Class = 1
 R_nu = 0
