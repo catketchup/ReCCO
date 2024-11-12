65cd754d9866ef6a84f0a4f13f0b3336797e1a66
Hongbo <ketchup@sjtu.edu.cn>
2024-11-12 19:35:38 +0800

    reformatting code

--- code/param.py
+++ code/param.py
@@ -4,27 +4,21 @@ import math
 #kms^-1
 c = 3.e5
 # Hubble parameter, kms^-1Mpc^-1
+
 H0 = 68
 # observable universe radius,14260 Mpc
-r_o = 15000
+
 # the boundary position of the two bubbles, Mpc
-x_c = 0
+z_c = 0
 r_H = c/H0 # Mpc
+direction = 'x'
+
 # initial potential from bubble collision psi_i parametried by A and B, Eq.2.2
-# A = 1.e-4 # 1-sigma limit
-# B = 0
+A = 1.e-4 # 1-sigma limit
+B = 0
 
 # A =  0# 1-sigma limit
 # B = 1.e-4
-
-# A = 0
-# B = 1.e-4 # 1-sigma limit
-
-# A = 1
-# B = 1
-# need a resolution of the 3-d grid?
-# d = 1000
-# N = int(2*r_o/d) +1
 
 curvature_ini_Class = 1
 R_nu = 0
