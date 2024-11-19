ccd6ff99741a6241d446ebb0659dae8a5811dc80
Hongbo <ketchup@sjtu.edu.cn>
2024-11-19 22:17:03 +0800

    fft bug

---
+++ code/my_tools.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import interp1d
+from math import pi
+
+
+def ThreeDEvolve(G_itp, input_g_field_k, kk):
+    kk[np.where(kk==0)] = kk.mean()
+    # here kk should be in 1/Mpc
+    output_field_k = G_itp(kk.flatten()).reshape(kk.shape)*input_g_field_k/kk
+    return np.fft.ifftn(output_field_k)
+
+def ThreeDEvolve_test(G_function, input_g_field_k, kk):
+    kk[np.where(kk==0)] = kk.mean()
+    # here kk should be in 1/Mpc
+    output_field_k = G_function*input_g_field_k/kk
+    return np.fft.ifftn(output_field_k)
+
+def ThreeDEvolve_component(G_function, input_field_k, kk_i, kk):
+    kk[np.where(kk==0)] = kk.mean()
+    # here kk should be in 1/Mpc
+    output_field_k = G_function*input_field_k*kk_i/kk
+    return np.fft.ifftn(output_field_k)
+
+def ThreeDField_on_TwoDSurface(x_grid, y_grid, z_grid, field3D, x, y, z):
+    return RegularGridInterpolator((x_grid, y_grid, z_grid), field3D)((y, x, z))
+    # return RegularGridInterpolator((x_grid, y_grid, z_grid), field3D)((x, y, z))
+
+def TwoDVecField_Radial(field_x1, field_x2, field_x3, theta, phi):
+    return field_x1*np.sin(theta)*np.cos(phi) + field_x2*np.sin(theta)*np.sin(phi) + field_x3*np.cos(theta)
