ac931f5272ad1bf281c59d6287edb27e6328fd3a
Hongbo <ketchup@sjtu.edu.cn>
2024-11-22 20:34:52 +0800

    writing functions

--- code/bubble_collision.py
+++ code/bubble_collision.py
@@ -3,9 +3,26 @@ from math import pi
 import my_remote_spectra as rs
 import config as config
 
-def Cos_theta_c(Z_c, z_e, chi_e, theta_e):
-    "Capital Z_c for comoving position of the boundary at z-direction by default"
-    chi_e = rs.chifromz(z_e)
-    chi_edec = rs.chifromz(config.zdec) - rs.chifromz(config(z_e))
-    return (Z_c -chi_e*np.cos(theta_e))/chi_edec
 
+class BubbleCollision_Veff():
+    def __init__(self, Z_e, Omega_b, Omega_c, w, wa, Omega_K, h):
+        self.Z_e = Z_e
+        self.Omega_b = Omega_b
+        self.Omega_c = Omega_c
+        self.w = w
+        self.wa = wa
+        self.Omega_K = Omega_K
+        self.h = h
+        self.chi_e = rs.chifromz(Z_e)
+        self.chi_edec = rs.chifromz(config.zdec) - rs.chifromz(Z_e)
+        self.r_H = 3.e5/(h*100)
+        self.Dpsi_dec = rs.Dpsi_inter(Omega_b, Omega_c, w, wa, Omega_K, h)(1/(1+config.zdec))
+
+    def Cos_theta_c(self, Z_c, theta_e):
+        return (Z_c -self.chi_e*np.cos(theta_e))/self.chi_edec
+
+    def Approx_Veff_SW_radial(self, A, B, Z_c, theta_e):
+        cos_theta_c = self.Cos_theta_c(Z_c, theta_e)
+        cos_theta_e = np.cos(theta_e)
+        # return  (2*self.Dpsi_dec-3/2)*3/2*cos_theta_e*(A/self.r_H*((self.chi_e*cos_theta_e-Z_c)*1/2*(cos_theta_c**2-1)+1/3*self.chi_edec*(cos_theta_c**3-1)))
+        return  (2*self.Dpsi_dec-3/2)*3/2*cos_theta_e*(A/self.r_H*((self.chi_e*cos_theta_e-Z_c)*1/2*(1-cos_theta_c**2)+1/3*self.chi_edec*(1-cos_theta_c**3)))
--- code/my_tools.py
+++ code/my_tools.py
@@ -5,10 +5,12 @@ from math import pi
 
 
 def ThreeDEvolve(G_itp, input_g_field_k, kk):
-    # kk[np.where(kk==0)] = kk.mean()
-    print(np.where(kk==0))
     # here kk should be in 1/Mpc
-    output_field_k = G_itp(kk.flatten()).reshape(kk.shape)*input_g_field_k/kk
+    zero_index = np.where(kk==0)
+    kk[zero_index] = 1
+    output_field_k = G_itp(kk)*input_g_field_k/kk
+    # output_field_k[zero_index] =
+
     return np.fft.ifftn(output_field_k)
 
 def ThreeDEvolve_test(G_function, input_g_field_k, kk):
